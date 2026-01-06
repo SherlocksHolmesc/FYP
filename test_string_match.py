6af625b30e8cd566ad553afa11bbdae8e6992db8
SherlocksHolmesc <144738101+SherlocksHolmesc@users.noreply.github.com>
2026-01-07 03:57:17 +0800

    delete unused stuff

--- test_string_match.py
+++
@@ -1,6 +0,0 @@
-msg = "Could not decode contract function call to balanceOf(address) with return data: b'', output_types: ['uint256']"
-print('Full message:', msg)
-print()
-print('Check 1:', "with return data: b''" in msg)
-print('Check 2:', "return data: b''" in msg)
-print('Check 3:', "b''" in msg)
